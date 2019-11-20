ab17f6105b76abbfc0a7325cbfbebe6165aad837
Clint J Edwards <clint.j.edwards@gmail.com>
2019-11-20 18:35:19 -0500

    Fixing graph generation for pipelines get

--- cmd/pipelinesGet.go
+++ cmd/pipelinesGet.go
@@ -44,7 +44,7 @@ func runPipelinesGetCmd(cmd *cobra.Command, args []string) {
 	fmt.Printf("Root Task: [%s] %s\n", pipeline.RootTaskId, pipeline.Tasks[pipeline.RootTaskId].Name)
 
 	taskGraph := treeprint.New()
-	taskGraph = populateTree(pipeline.RootTaskId, pipeline.Tasks, taskGraph)
+	populateTree(pipeline.RootTaskId, pipeline.Tasks, taskGraph)
 	fmt.Printf("\nTask Graph:\n")
 	fmt.Println(taskGraph.String())
 }
@@ -55,20 +55,19 @@ func init() {
 
 // populateTree is a recursive function that parses a graph map and turns it into
 // tree graph
-func populateTree(nodeID string, taskMap map[string]*api.Task, taskTree treeprint.Tree) treeprint.Tree {
+func populateTree(nodeID string, taskMap map[string]*api.Task, taskTree treeprint.Tree) {
 	if _, ok := taskMap[nodeID]; !ok {
-		return taskTree
+		return
 	}
 
 	if len(taskMap[nodeID].Children) == 0 {
-		return taskTree.AddNode(taskMap[nodeID].Name)
+		taskTree.AddNode(taskMap[nodeID].Name)
+		return
 	}
 
 	taskTree = taskTree.AddBranch(taskMap[nodeID].Name)
 
 	for _, child := range taskMap[nodeID].Children {
-		taskTree = populateTree(child, taskMap, taskTree)
+		populateTree(child, taskMap, taskTree)
 	}
-
-	return taskTree
 }
